3f73ed406c59152e9ec37e03dd3541e3ab449bd1
Haruna Salim <salxsaa@gmail.com>
2016-12-18 00:12:24 +0100

    Added the units controller... Added the units views....

---
+++ Opmas/Controllers/ApplicationManagement/UnitsController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Opmas.Data.DataContext.DataContext.SystemDataContext;
+using Opmas.Data.Objects.Entities.SystemManagement;
+
+namespace Opmas.Controllers.ApplicationManagement
+{
+    public class UnitsController : Controller
+    {
+        private UnitDataContext db = new UnitDataContext();
+
+        // GET: Units
+        public ActionResult Index(long id)
+        {
+            var units = db.Units.Include(u => u.DepartmentId == id);
+            return View(units.ToList());
+        }
+
+        // GET: Units/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Unit unit = db.Units.Find(id);
+            if (unit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(unit);
+        }
+
+        // GET: Units/Create
+        public ActionResult Create(long? id)
+        {
+            //ViewBag.DepartmentId = new SelectList(db.Departments, "DepartmentId", "Name");
+            ViewBag.DepartmentId = id;
+            return View();
+        }
+
+        // POST: Units/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UnitId,Name,Description")] Unit unit,FormCollection collectedValues)
+        {
+            if (ModelState.IsValid)
+            {
+                unit.DepartmentId = Convert.ToInt64(collectedValues["DepartmentId"]);
+                db.Units.Add(unit);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DepartmentId = new SelectList(db.Departments, "DepartmentId", "Name", unit.DepartmentId);
+            return View(unit);
+        }
+
+        // GET: Units/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Unit unit = db.Units.Find(id);
+            if (unit == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DepartmentId = new SelectList(db.Departments, "DepartmentId", "Name", unit.DepartmentId);
+            return View(unit);
+        }
+
+        // POST: Units/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UnitId,Name,Description,DepartmentId")] Unit unit)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(unit).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DepartmentId = new SelectList(db.Departments, "DepartmentId", "Name", unit.DepartmentId);
+            return View(unit);
+        }
+
+        // GET: Units/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Unit unit = db.Units.Find(id);
+            if (unit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(unit);
+        }
+
+        // POST: Units/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            Unit unit = db.Units.Find(id);
+            db.Units.Remove(unit);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
