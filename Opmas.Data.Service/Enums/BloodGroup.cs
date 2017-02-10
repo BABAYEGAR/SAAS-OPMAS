36937a5696d739b6519a713193c5567052db9ec3
Haruna Salim <salxsaa@gmail.com>
2017-02-10 06:08:14 +0100

    Modified enum class

--- Opmas.Data.Service/Enums/BloodGroup.cs
+++ Opmas.Data.Service/Enums/BloodGroup.cs
@@ -1,11 +1,25 @@
-﻿namespace Opmas.Data.Service.Enums
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Opmas.Data.Service.Enums
 {
     public enum BloodGroup
     {
-        A,
-        B,
-        AB,
-        O
+        [Display(Name = "O+")]
+        Opositive,
+        [Display(Name = "O-")]
+        Onegative,
+        [Display(Name = "A+")]
+        Apositive,
+        [Display(Name = "A-")]
+        Anegative,
+        [Display(Name = "B+")]
+        Bpositive,
+        [Display(Name = "B-")]
+        Bnegative,
+        [Display(Name = "AB+")]
+        ABpositive,
+        [Display(Name = "AB-")]
+        ABnegative
 
     }
 }
